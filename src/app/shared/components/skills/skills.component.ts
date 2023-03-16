bb15d2c289ddf22f33b630bf51eb38aed57d9538
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-03-16 12:38:15 +0400

    create skills array

---
+++ src/app/shared/components/skills/skills.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss']
+})
+export class SkillsComponent {
+
+  skills = [
+    {
+      name : 'HTML',
+      imagePath : './assets/icons/html.png'
+    },
+    {
+      name : 'CSS',
+      imagePath : './assets/icons/css-3.png'
+    },
+    {
+      name : 'SCSS',
+      imagePath : './assets/icons/scss.png'
+    },
+    {
+      name : 'Bootstrap',
+      imagePath : './assets/icons/bootstrap.png'
+    },
+    {
+      name : 'JavaScript',
+      imagePath : './assets/icons/javascript.png'
+    },
+    {
+      name : 'TypeScript',
+      imagePath : './assets/icons/typescript.png'
+    },
+    {
+      name : 'Angular',
+      imagePath : './assets/icons/angular.png'
+    },
+    {
+      name : 'React.js',
+      imagePath : './assets/icons/react.png'
+    },
+    {
+      name : 'Redux',
+      imagePath : './assets/icons/redux.png'
+    },
+    {
+      name : 'PHP',
+      imagePath : './assets/icons/php.png'
+    },
+    {
+      name : 'WordPress',
+      imagePath : './assets/icons/wordpress.png'
+    },
+    {
+      name : 'Flutter',
+      imagePath : './assets/icons/Flutter.png'
+    },
+    {
+      name : 'Adobe-Photoshop',
+      imagePath : './assets/icons/adobe-photoshop.png'
+    },
+    {
+      name : 'Adobe-Illustrator',
+      imagePath : './assets/icons/adobe-illustrator.png'
+    },
+    {
+      name : 'Figma',
+      imagePath : './assets/icons/figma.png'
+    },
+    {
+      name : 'MC Office',
+      imagePath : './assets/icons/Microsoft-Office.png'
+    },
+  ]
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './components/header/header.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
     declarations : [
         HeaderComponent,
+        SkillsComponent,
     ],
     imports : [
 
