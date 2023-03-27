e3f512dfcb922123938e9795de8b2bad28dba54e
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-03-27 17:05:25 +0400

    make main page responsive

---
+++ src/app/shared/components/projects-container/projects-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-container',
+  templateUrl: './projects-container.component.html',
+  styleUrls: ['./projects-container.component.scss']
+})
+export class ProjectsContainerComponent {
+
+}
